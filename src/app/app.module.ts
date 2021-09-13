15f0a32677e645e5d710a6d00f608f7210535800
Max Kozubenko <max.kozubenko4@gmail.com>
2021-09-13 15:28:31 +0300

    change directory from components to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { AppComponent } from './app.component';
-import { LoginModule } from './components/login-page/login.module';
-import { ProfileModule } from './components/profile-page/profile.module';
-import { LibraryModule } from './components/library-page/library.module';
-import { FriendsModule } from './components/friends-page/friends.module';
-import { GamesModule } from './components/games-page/games.module';
+import { LoginModule } from './modules/login-page/login.module';
+import { ProfileModule } from './modules/profile-page/profile.module';
+import { LibraryModule } from './modules/library-page/library.module';
+import { FriendsModule } from './modules/friends-page/friends.module';
+import { GamesModule } from './modules/games-page/games.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { GamesGuard } from './components/games-page/games.guard';
+import { GamesGuard } from './modules/games-page/games.guard';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
