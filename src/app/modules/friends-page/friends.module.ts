23eea3a4eeac7981681cc9bf3e7b0b14f053d7e6
Max Kozubenko <max.kozubenko4@gmail.com>
2021-09-14 20:59:41 +0300

    delete user-block component

--- src/app/modules/friends-page/friends.module.ts
+++ src/app/modules/friends-page/friends.module.ts
@@ -5,13 +5,11 @@ import { FriendsRoutingModule } from './friends-routing.module';
 import { FriendsComponent } from './friends.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { UserBlockComponent } from './components/user-block/user-block.component';
 
 
 @NgModule({
   declarations: [
     FriendsComponent,
-    UserBlockComponent
   ],
   imports: [
     CommonModule,
