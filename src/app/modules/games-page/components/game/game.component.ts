8a02d200b66aca03d209c4221c3c3d155c9515d2
Max Kozubenko <max.kozubenko4@gmail.com>
2021-09-16 00:02:12 +0300

    after lint fix

--- src/app/modules/games-page/components/game/game.component.ts
+++ src/app/modules/games-page/components/game/game.component.ts
@@ -1,14 +1,14 @@
 import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 import { GameInterface } from 'src/app/models/model';
 
-
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-game',
+    templateUrl: './game.component.html',
+    styleUrls: ['./game.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GameComponent {
-  @Input() games:GameInterface[] = [];
-  @Output() addGameUser = new EventEmitter()
+    @Input() games: GameInterface[] = [];
+
+    @Output() addGameUser = new EventEmitter();
 }
